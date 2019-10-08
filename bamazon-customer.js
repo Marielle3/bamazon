3e2799a9975c4c9bfef70787f76c4ddfe40a040a
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-10-07 20:11:12 -0700

    functions to be able to see products, still need to create go shopping function

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -8,3 +8,30 @@ var connection = mysql.createConnection({
   password: "Jospar3233!",
   database: "bamazon"
 });
+connection.connect(function(err) {
+  if (err) throw err;
+  // runSearch();
+});
+
+seeProducts().then(function(result){
+  result.forEach(function(products){
+    console.log('\nItem ID: ' + products.item_id + '\nProduct: ' + products.product_name + '\nDepartment: ' + products.department_name  + '\nPrice: ' + products.price + '\nStock Quantity: ' + products.stock_quantity);
+  });
+  .then(function(){
+    return goShopping();
+  });
+});
+
+function seeProducts() {
+    return new Promise(function(resolve, reject) {
+        // query for all items in products table
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) reject(err);
+            resolve(res);
+        });
+    });
+}
+connection.end();
+seeProducts();
+
+//TODO create goShopping function 
